c3e4242a102b51840391c9947d60c29fe40ae4bf
Mxolisi Sangweni <mxsangwjhb024@student.wethinkcode.co.za>
2025-10-06 11:07:25 +0200

    .

---
+++ tests/test_exercise_2.py
@@ -0,0 +1,14 @@
+import unittest
+
+class test_exercise(unittest.TestCase):
+    def test_fizz_buzz(self):
+        pass
+    
+    def test_is_palindrome(self):
+        pass
+    
+    def test_factorial(self):
+        pass
+    
+    def test_are_anagrams(self):
+        pass
